01703b3c378f9adff0110079dd6183ea4a8ceba1
Errol <errolmcfadden@hotmail.com>
2026-05-20 00:06:18 +1000

    - Added circle inside icon

--- Flicksy.Editor/Properties/Resources.Designer.cs
+++ Flicksy.Editor/Properties/Resources.Designer.cs
@@ -83,6 +83,16 @@ namespace Flicksy.Editor.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap circle_inside {
+            get {
+                object obj = ResourceManager.GetObject("circle_inside", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap cursor {
             get {
                 object obj = ResourceManager.GetObject("cursor", resourceCulture);
